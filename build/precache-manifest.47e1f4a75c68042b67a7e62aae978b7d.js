9f42c82883c13b219e4df73c3c6b449c9a482fdb
Israr Ali <israr.ali@systemsltd.com>
2021-01-13 17:18:11 +0500

    Third commit

---
+++ build/precache-manifest.47e1f4a75c68042b67a7e62aae978b7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2961bf506b370fc716db7a4c888da35",
+    "url": "/Quiz_App/index.html"
+  },
+  {
+    "revision": "695017709b1eb30a88ec",
+    "url": "/Quiz_App/static/css/main.d39fdce4.chunk.css"
+  },
+  {
+    "revision": "5a9319e839112aa29511",
+    "url": "/Quiz_App/static/js/2.16095cef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Quiz_App/static/js/2.16095cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695017709b1eb30a88ec",
+    "url": "/Quiz_App/static/js/main.f947f7ce.chunk.js"
+  },
+  {
+    "revision": "24c8e09a28ec39fb2222",
+    "url": "/Quiz_App/static/js/runtime-main.6deb85d7.js"
+  }
+]);
